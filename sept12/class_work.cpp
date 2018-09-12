38c2f392cb443860eb22d405c1fd9be5a37eab6c
Alex Blanke <alexblanke98@gmail.com>
2018-09-12 16:41:21 -0500

    added looping

--- sept12/class_work.cpp
+++ sept12/class_work.cpp
@@ -16,12 +16,32 @@ int main()
 	cin >> side2;
 	cout << endl;
 
-	//Calculate
-	calc_sides = pow(side1, 2) + pow(side2, 2);
-	side3 = pow(calc_sides, 0.5);
+	//Loop until a negative is entered for side1
+	while (side1 >= 0)
+	{
 
-	//Output
-	cout << "The Hypotenuse is : " << side3 << ".";
+		//Calculate
+		calc_sides = pow(side1, 2) + pow(side2, 2);
+		side3 = pow(calc_sides, 0.5);
+
+		//Output
+		cout << "The Hypotenuse is : " << side3 << endl << endl;
+		cout << "New triangle. Enter a negative value for first side to exit."
+			<< endl;
+
+		//Ask for user input
+		cout << "Enter length of first side : ";
+		cin >> side1;
+		cout << endl;
+		if (side1 < 0)
+		{
+			break;
+		}
+
+		cout << "Enter length of second side : ";
+		cin >> side2;
+		cout << endl;
+	}
 
 	return 0;
 }
